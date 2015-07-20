55cb417abd0fb3c339291252fc58c61b493fb827
liuuniverse <liuuniverse@163.com>
2015-07-20 23:03:39 +0800

    add_three

--- hello.php
+++ hello.php
@@ -1,3 +1,17 @@
 <?php
 	echo 'hello world';
+	switch($sex){
+		case 1:
+			echo '还好不是人妖';
+			break;
+		case 2:
+			echo '还好不是人妖';
+			break;
+		case 3:
+			echo '还好不是人妖';
+			break;
+		default:
+			echo '没错，你就是人妖';
+			break;
+	}
 ?>
